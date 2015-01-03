1c5bfbd39cfa8e581a68c3c672174698181cb6f6
lindvi <lindvi@kth.se>
2015-01-03 18:49:04 +0100

    Changed to request to @freddans server, which allows CORS

--- app/services/searchFactory.js
+++ app/services/searchFactory.js
@@ -4,11 +4,11 @@ komFramISLApp.factory('searchFactory', ['$http',
 
         var searchResult;
         var searchFn = function (from, to, successCb, errorCb) {
-            $http.get('http://komframisl.my.local/api2/TravelplannerV2/trip.json?key=6a517447db2c4a72adc256399cef82ad&originId=' + from + '&destId=' + to)
+            $http.get('http://fredriklowenhamn.se/slappi/api/sl?origin=' + from + '&destination=' + to)
+            //$http.get('http://localhost:8080/api2/TravelplannerV2/trip.json?key=6a517447db2c4a72adc256399cef82ad&originId=' + from + '&destId=' + to)
             /*$http.get('http://komframisl.my.local/fidde/sl?origin=' + from + '&destination=' + to)*/
-
-
             .success(function (data, status, headers, config) {
+                data = JSON.parse(data);
                 _.each(data.TripList.Trip, function (trip) {
 
                     if (!angular.isArray(trip.LegList.Leg)) {
