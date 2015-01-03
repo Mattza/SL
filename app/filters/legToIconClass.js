0ce2757eb6d4bfda7554949df6ff9a8faff3cbfe
Mattza <matknu@kth.se>
2015-01-03 21:04:28 +0100

    Fixar historik och stationer med hårdkodadbackend

--- app/filters/legToIconClass.js
+++ app/filters/legToIconClass.js
@@ -2,18 +2,18 @@ komFramISLApp.filter('legToIconClass', function () {
     return function (leg) {
         var temp = {
             METRO: {
+                10: 'MET_blue',
                 11: 'MET_blue',
-                12: 'MET_blue',
                 13: 'MET_red',
                 14: 'MET_red',
-                15: 'MET_red',
-                16: 'MET_green',
                 17: 'MET_green',
-                18: 'MET_green'
+                18: 'MET_green',
+                19: 'MET_green'
             },
-            BUS: {
-                610: 'BUS_red'
-            }
+            BUS: 'BUS_red',
+            TRAM: 'TRM',
+            WALK: 'WALK',
+            TRAIN: 'TRAIN'
 
         };
         var ret = temp[leg.type];
--- app/services/searchFactory.js
+++ app/services/searchFactory.js
@@ -4,27 +4,21 @@ komFramISLApp.factory('searchFactory', ['$http',
 
         var searchResult;
         var searchFn = function (from, to, successCb, errorCb) {
-            $http.get('http://komframisl.my.local/api2/TravelplannerV2/trip.json?key=6a517447db2c4a72adc256399cef82ad&originId=' + from + '&destId=' + to)
-            /*$http.get('http://komframisl.my.local/fidde/sl?origin=' + from + '&destination=' + to)*/
+            $http.get('http://fredriklowenhamn.se/slappi/api/sl/?origin=' + from + '&destination=' + to)
+                .success(function (data, status, headers, config) {
+                    _.each(data.TripList.Trip, function (trip) {
 
-
-            .success(function (data, status, headers, config) {
-                _.each(data.TripList.Trip, function (trip) {
-
-                    if (!angular.isArray(trip.LegList.Leg)) {
-                        var temp = trip.LegList.Leg;
-                        trip.LegList.Leg = [temp];
-                    }
-                    trip.StartTime = trip.LegList.Leg[0].Origin.time;
-                    trip.ArrivalTime = trip.LegList.Leg[trip.LegList.Leg.length - 1].Destination.time;
-                    _.each(trip.LegList.Leg, function (leg) {
-                        leg.samePlace = leg.Origin.name === leg.Destination.name;
+                        if (!angular.isArray(trip.LegList.Leg)) {
+                            var temp = trip.LegList.Leg;
+                            trip.LegList.Leg = [temp];
+                        }
+                        trip.StartTime = trip.LegList.Leg[0].Origin.time;
+                        trip.ArrivalTime = trip.LegList.Leg[trip.LegList.Leg.length - 1].Destination.time;
                     });
-                });
-                searchResult = data;
-                successCb(data);
+                    searchResult = data;
+                    successCb(data);
 
-            })
+                })
                 .error(function () {
                     errorCb();
                 });
